1aa21139e6d6bcfe186fd00ee9838861155d09f2
Moongoding <Krismamuldan@gmail.com>
2022-06-12 18:05:16 +0700

    sc commit

---
+++ application/views/landingpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/icon/css/boxicons.min.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/icon/css/animations.css');?>">
+
+    <title>Undangan</title>
+</head>
+
+<body>
+
+
+    <H1> INI TAMPILAN UDAH BISA</H1>
+
+
+
+    <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.min.js');?>"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
